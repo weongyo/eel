2e9df9ee62302d41edeb03805f1b264bd410ef88
Weongyo Jeong <wjeong@barracuda.com>
2013-11-08 17:24:26 -0800

    UP.

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-NSPR_ROOT=/home/weongyo/Sources/eel/vendor/nsprpub/dist
+TOPDIR=/home/weongyo/Sources/eel
+NSPR_ROOT=$TOPDIR/vendor/nsprpub/dist
+
+cd vendor/c-ares-1.10.0 && ./configure --prefix=/opt/eel-1.0.0 && \
+    make && make install
+
+cd vendor/curl-7.33.0 && ./configure --prefix=/opt/eel-1.0.0 \
+    --enable-debug --disable-optimize --enable-ares=/opt/eel-1.0.0 && \
+    make && make install
+
+cd vendor/gumbo-parser && ./configure --prefix=/opt/eel-1.0.0 \
+    --enable-debug --disable-optimize && make && make install
 
 ./configure --prefix=/opt/eel-1.0.0 \
     --enable-64bit --enable-debug --disable-optimize \
