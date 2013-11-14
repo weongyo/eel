3a8cc0a8a99487572366501300f6a33f755bd3d2
Weongyo Jeong <wjeong@barracuda.com>
2013-11-13 18:06:15 -0800

    Up.

--- src/eel_js.cc
+++ src/eel_js.cc
@@ -4,13 +4,42 @@
 
 #include "eel.h"
 
+#define DEFAULT_MAX_STACK_SIZE 500000
+static size_t gMaxStackSize = DEFAULT_MAX_STACK_SIZE;
 static unsigned gStackBaseThreadIndex;
 
-static JSRuntime *rt;
+static PRLock *gWatchdogLock = NULL;
+static PRCondVar *gWatchdogWakeup = NULL;
+static PRThread *gWatchdogThread = NULL;
+static bool gWatchdogHasTimeout = false;
+static int64_t gWatchdogTimeout = 0;
+
+static PRCondVar *gSleepWakeup = NULL;
+
+static JSRuntime *gRuntime = NULL;
+
+static bool
+InitWatchdog(JSRuntime *rt)
+{
+	JS_ASSERT(!gWatchdogThread);
+	gWatchdogLock = PR_NewLock();
+	if (gWatchdogLock) {
+		gWatchdogWakeup = PR_NewCondVar(gWatchdogLock);
+		if (gWatchdogWakeup) {
+			gSleepWakeup = PR_NewCondVar(gWatchdogLock);
+			if (gSleepWakeup)
+				return true;
+			PR_DestroyCondVar(gWatchdogWakeup);
+		}
+		PR_DestroyLock(gWatchdogLock);
+	}
+	return false;
+}
 
 int
 EJS_init(void)
 {
+	JSRuntime *rt;
 	PRStatus ret;
 	int stackDummy;
 
@@ -23,5 +52,10 @@ EJS_init(void)
 	rt = JS_NewRuntime(32L * 1024L * 1024L);
 	if (!rt)
 		return (-1);
+	JS_SetGCParameter(rt, JSGC_MAX_BYTES, 0xffffffff);
+	JS_SetNativeStackQuota(rt, gMaxStackSize);
+	if (!InitWatchdog(rt))
+		return (-1);
+	gRuntime = rt;
         return (0);
 }
